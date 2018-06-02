31ac04a12d768b8da28ea4ba0539d7b43cba7229
Romain Braun <romain@oldstlabs.com>
2018-06-02 12:34:32 +0100

    Optimize css bundling for production

--- webpack.config.js
+++ webpack.config.js
@@ -4,69 +4,73 @@ const HtmlWebpackPlugin  = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ManifestPlugin     = require('webpack-manifest-plugin');
 const CopyWebpackPlugin  = require('copy-webpack-plugin')
-const WriteFilePlugin = require('write-file-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-
-module.exports = {
-  entry: {
-    app: './src/index.ts'
-  },
-  devtool: 'eval',
-  mode: 'development',
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-    symlinks: false
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.html$/,
-        use: 'html-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist/'),
-    hot: true
-  },
-  plugins: [
-    new ManifestPlugin(),
-    new CleanWebpackPlugin(['dist']),
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Alain Blondel, peintre',
-      template: './src/index.html'
-    }),
-    new CopyWebpackPlugin([{
-      from: 'src/assets/',
-      to: 'assets'
-    }]),
-    // new WriteFilePlugin()
-  ],
+module.exports = function(env) {
+  return {
+    entry: {
+      app: './src/index.ts'
+    },
+    devtool: 'eval',
+    mode: 'development',
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+      symlinks: false
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            !env.production ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader'
+          ],
+          exclude: /node_modules/
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            'file-loader'
+          ],
+          exclude: /node_modules/
+        },
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.html$/,
+          use: 'html-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, 'dist/'),
+      hot: true
+    },
+    plugins: [
+      new ManifestPlugin(),
+      new CleanWebpackPlugin(['dist']),
+      new webpack.NamedModulesPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+      new HtmlWebpackPlugin({
+        title: 'Alain Blondel, peintre',
+        template: './src/index.html'
+      }),
+      new CopyWebpackPlugin([{
+        from: 'src/assets/',
+        to: 'assets'
+      }]),
+      new MiniCssExtractPlugin({
+        filename: !env.production ? '[name].css' : '[name].[hash].css',
+        chunkFilename: !env.production ? '[id].css' : '[id].[hash].css',
+      })
+    ],
+  };
 };
